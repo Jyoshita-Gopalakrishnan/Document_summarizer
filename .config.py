1090b10df2654c909b15ba5a15226f59845dae1b
Jyoshita-Gopalakrishnan <jyoshitakgk2002@gmail.com>
2025-03-12 18:55:33 +0530

    Update and rename .config.py to config.py

--- .config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-# OpenAI API Key (set as an environment variable for security)
-OPENAI_API_KEY = os.environ.get("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")
---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+# OpenAI API Key (set as an environment variable for security)
+OPENAI_API_KEY = os.environ.get("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")
